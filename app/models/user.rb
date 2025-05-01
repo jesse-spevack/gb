0cfeaa3186b74484cddd7c166d7912591ec3d248
Jesse Spevack <jspevack@gmail.com>
2025-05-01 15:04:45 -0600

    Feature/task 5 create user model and migration 2 (#3)

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# User model
+#
+# Schema Information
+#
+# Table name: users
+#
+# id                   :integer          not null, primary key
+# email                :string           not null, indexed, unique
+# name                 :string           not null
+# google_uid           :string           not null, indexed, unique
+# profile_picture_url  :string
+# admin                :boolean          default(false)
+# created_at           :datetime         not null
+# updated_at           :datetime         not null
+#
+class User < ApplicationRecord
+  has_prefix_id :usr
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true
+  validates :google_uid, presence: true, uniqueness: true
+
+  def admin?
+    admin
+  end
+end
