cc30b931a33973f72ff85e5d8749c417c88b18ca
Jesse Spevack <jspevack@gmail.com>
2025-05-02 16:27:36 -0600

    Feature: Create user token model and migration - Task 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_01_210945) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_02_215826) do
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "user_agent", null: false
@@ -18,6 +18,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_01_210945) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "user_tokens", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "access_token", null: false
+    t.string "refresh_token", null: false
+    t.datetime "expires_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_tokens_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +43,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_01_210945) do
   end
 
   add_foreign_key "sessions", "users"
+  add_foreign_key "user_tokens", "users"
 end
